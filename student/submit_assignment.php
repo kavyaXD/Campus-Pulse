ec3073f7276364bbf862015c215c2b19114c3f53
kavyaXD <130041996+kavyaXD@users.noreply.github.com>
2024-08-01 13:19:31 +0530

    main

--- student/submit_assignment.php
+++ student/submit_assignment.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$title='';
+$title = 'Submit Assignment';
 require '../vendor/autoload.php';
 require '../config/db.php';
 
@@ -15,30 +15,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $file_path = $_FILES['file_path']['name'];
     $submission_date = date('Y-m-d');
 
-    move_uploaded_file($_FILES['file_path']['tmp_name'], "uploads/$file_path");
+    // Move uploaded file to the current directory
+    move_uploaded_file($_FILES['file_path']['tmp_name'], $file_path);
 
+    // Insert submission into the database
     $qry = "INSERT INTO submission (assignment_id, user_id, file_path, submission_date) VALUES ('$assignment_id', '$student_id', '$file_path', '$submission_date')";
     mysqli_query($con, $qry);
     header('location:index.php');
 }
 
+// Fetch all assignments for display
+$qry = "SELECT * FROM assignments;";
+$result = mysqli_query($con, $qry);
+
 ob_start();
 ?>
 
-<div class="col-md-10">
+<div class="container-fluid">
     <div class="row">
-        <h2>Submit Assignment</h2>
-        <form action="submit_assignment.php" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="assignment_id">Assignment ID:</label>
-                <input type="number" class="form-control" id="assignment_id" name="assignment_id" required>
-            </div>
-            <div class="form-group">
-                <label for="file_path">Upload File:</label>
-                <input type="file" class="form-control" id="file_path" name="file_path" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit Assignment</button>
-        </form>
+        <div class="col-12">
+            <h2>Assignments</h2>
+            <table class="table table-hover table-striped table-bordered mb-0">
+                <thead class="bg-dark text-white text-center">
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Due Date</th>
+                        <th>Upload File</th>
+                        <th>Submit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                        <tr>
+                            <td><?php echo $row['title']; ?></td>
+                            <td><?php echo $row['description']; ?></td>
+                            <td><?php echo $row['due_date']; ?></td>
+                            <td class="text-center">
+                                <form action="submit_assignment.php" method="post" enctype="multipart/form-data">
+                                    <input type="hidden" name="assignment_id" value="<?php echo $row['id']; ?>">
+                                    <div class="form-group">
+                                        <input type="file" name="file_path" required>
+                                    </div>
+                                    <td class="text-center">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    </td>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
